ff21092bb3b8aeaa84c84569038def04501dba99
sheikrayhan <aheikrayhan39@gmail.com>
2023-02-23 08:29:09 +0600

    version 1

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+$(function () {
+   'use strict';
+   // Menu Fixed...
+   $(window).on('scroll', function () {
+      var scrollSize = $(window).scrollTop();
+      
+      if (scrollSize > 100) {
+         $('#navbar').addClass('menuActive');
+      } else {
+         $('#navbar').removeClass('menuActive');
+      }
+   })
+   $('.backtoTop').on('click', function () {
+      $('html, body').animate({
+          scrollTop: 0
+      })
+   })
+
+   // PopUp Js...
+   $('#searchForm').on('click', function () {
+      $('#spopup').show(500);
+   })
+   $('.removeIcon').on('click', function () {
+      $('#spopup').hide(500);
+   })
+
+   // Side Menu js
+    $('#sideBar').on('click', function () {
+         $('.sideMenu').show(200);
+         $('.sideMenu').addClass('sidemenuActive')
+      })
+      $('.hide_btn').on('click', function () {
+         $('.sideMenu').hide(200)
+         $('.sideMenu').removeClass('sidemenuActive')
+    })
+   
+   // Portfolio Slider Initialize...
+   $('.insprtnSlider').slick({
+      arrows: false,
+      slidesToShow: 2,
+      autoplay: true,
+      autoplaySpeed: 2500,
+   });
+
+   $('.celebrationSlider').slick({
+      arrows: false,
+      slidesToShow: 1,
+      autoplay: true,
+      autoplaySpeed: 2500,
+   });
+
+
+})
